5db77bdb1d7067116b021f503605f5e2063940f9
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2023-08-05 17:11:28 +0500

    passing dispatch as a Prop

--- colors-app/src/color-reducer.ts
+++ colors-app/src/color-reducer.ts
@@ -18,20 +18,22 @@ type ColorState = {
   hexColor: string;
 };
 
+export type AdjustColorActions = UpdateHexColor | UpdateRGBColorAction;
+
 export const initialState: ColorState = {
   hexColor: '#BADA55',
 };
 
 export const colorReducer = (
   state: ColorState = initialState,
-  action: UpdateHexColor | UpdateRGBColorAction,
+  action: AdjustColorActions,
 ) => {
   if (action.type === 'update-hex-color') {
     const { hexColor } = action.payload;
     return { ...state, hexColor };
   }
   if (action.type === 'update-rgb-color') {
-    const hexColor = rgb.hex(action.payload.rgb);
+    const hexColor = '#' + rgb.hex(action.payload.rgb);
     return { ...state, hexColor: hexColor };
   }
 
--- colors-app/src/components/adjust-colors/index.tsx
+++ colors-app/src/components/adjust-colors/index.tsx
@@ -3,16 +3,19 @@ import HexToCMYK from './to-cmyk';
 import HexToHSL from './to-hsl';
 import HexToHSV from './to-hsv';
 import HexToRGB from './to-rgb';
+import { Dispatch } from 'react';
+import { AdjustColorActions } from '../../color-reducer';
 
 type AdjustColorsProps = {
   hexColor: string;
+  dispatch: Dispatch<AdjustColorActions>;
 };
 
-const AdjustColors = ({ hexColor }: AdjustColorsProps) => {
+const AdjustColors = ({ hexColor, dispatch }: AdjustColorsProps) => {
   return (
-    <div className="flex flex-col w-full gap-4">
+    <div className="flex w-full flex-col gap-4">
       <h3>Adjust Colors</h3>
-      <HexToRGB hexColor={hexColor} />
+      <HexToRGB dispatch={dispatch} hexColor={hexColor} />
       <HexToHSL hexColor={hexColor} />
       <HexToHSV hexColor={hexColor} />
       <HexToCMYK hexColor={hexColor} />
