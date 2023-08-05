65a2ba434bcf5c36db5b6e15d59d826b0c009a42
Ibrohim Fayzullayev <ibrohim_f01@mail.ru>
2023-08-05 12:49:14 +0500

    creating actions for colorReducer

---
+++ colors-app/src/color-reducer.ts
@@ -0,0 +1,39 @@
+import { rgb } from 'color-convert';
+
+export type UpdateHexColor = {
+  type: 'update-hex-color';
+  payload: {
+    hexColor: string;
+  };
+};
+
+type UpdateRGBColorAction = {
+  type: 'update-rgb-color';
+  payload: {
+    rgb: [number, number, number];
+  };
+};
+
+type ColorState = {
+  hexColor: string;
+};
+
+export const initialState: ColorState = {
+  hexColor: '#BADA55',
+};
+
+export const colorReducer = (
+  state: ColorState = initialState,
+  action: UpdateHexColor | UpdateRGBColorAction,
+) => {
+  if (action.type === 'update-hex-color') {
+    const { hexColor } = action.payload;
+    return { ...state, hexColor };
+  }
+  if (action.type === 'update-rgb-color') {
+    const hexColor = rgb.hex(action.payload.rgb);
+    return { ...state, hexColor: hexColor };
+  }
+
+  return state;
+};
---
+++ colors-app/src/components/application.tsx
@@ -0,0 +1,29 @@
+import { useReducer } from 'react';
+import SavedColors from './saved-colors';
+import RelatedColors from './related-colors';
+import AdjustColors from './adjust-colors';
+import ColorPicker from './color-picker';
+import { colorReducer, initialState } from '../color-reducer';
+
+const Application = () => {
+  const [{ hexColor }, dispatch] = useReducer(colorReducer, initialState);
+
+  return (
+    <div className="mx-auto grid max-w-3xl grid-cols-1 gap-8 p-8 pb-40 dark:bg-slate-900 dark:text-white sm:grid-cols-2">
+      <ColorPicker
+        hexColor={hexColor}
+        onChange={(e) =>
+          dispatch({
+            type: 'update-hex-color',
+            payload: { hexColor: e.target.value },
+          })
+        }
+      />
+      <AdjustColors hexColor={hexColor} />
+      <RelatedColors hexColor={hexColor} />
+      <SavedColors hexColor={hexColor} />
+    </div>
+  );
+};
+
+export default Application;
